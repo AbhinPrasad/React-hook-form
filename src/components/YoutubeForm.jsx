676571185b1c9fde8a3a3301da40c4bd05b16f63
Abhin34 <107907575+Abhin34@users.noreply.github.com>
2023-04-13 11:49:08 +0530

    input tracking success - enabled devtools from hookform

--- src/components/YoutubeForm.jsx
+++ src/components/YoutubeForm.jsx
@@ -1,14 +1,37 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import { DevTool } from "@hookform/devtools";
 
 const YoutubeForm = () => {
+	const form = useForm();
+
+	const { register, control } = form;
+	// const { name, ref, onChange, onBlur } = register("username");
+
 	return (
 		<div>
 			<form>
-				<input type="text" name="username" placeholder="username" />
-				<input type="email" name="email" placeholder="email" />
-				<input type="text" name="channel" placeholder="channel" />
+				<input
+					type="text"
+					id="username"
+					placeholder="username"
+					{...register("username")}
+				/>
+				<input
+					type="email"
+					id="email"
+					placeholder="email"
+					{...register("email")}
+				/>
+				<input
+					type="text"
+					id="channel"
+					placeholder="channel"
+					{...register("channel")}
+				/>
 				<button>Submit</button>
 			</form>
+			<DevTool control={control} />
 		</div>
 	);
 };
