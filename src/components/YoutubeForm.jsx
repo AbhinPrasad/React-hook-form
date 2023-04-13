6dac643eff8257f351318a5cff953309be108bc1
Abhin34 <107907575+Abhin34@users.noreply.github.com>
2023-04-13 12:39:11 +0530

    form validation

--- src/components/YoutubeForm.jsx
+++ src/components/YoutubeForm.jsx
@@ -5,7 +5,9 @@ import { DevTool } from "@hookform/devtools";
 const YoutubeForm = () => {
 	const form = useForm();
 
-	const { register, control, handleSubmit } = form;
+	const { register, control, handleSubmit, formState } = form;
+
+	const { errors } = formState;
 
 	const onSubmit = (data) => {
 		console.log("submitted", data);
@@ -13,25 +15,54 @@ const YoutubeForm = () => {
 
 	return (
 		<div>
-			<form onSubmit={handleSubmit(onSubmit)}>
-				<input
-					type="text"
-					id="username"
-					placeholder="username"
-					{...register("username")}
-				/>
-				<input
-					type="email"
-					id="email"
-					placeholder="email"
-					{...register("email")}
-				/>
-				<input
-					type="text"
-					id="channel"
-					placeholder="channel"
-					{...register("channel")}
-				/>
+			<form onSubmit={handleSubmit(onSubmit)} noValidate>
+				<div className="form-control">
+					<input
+						type="text"
+						id="username"
+						placeholder="username"
+						{...register("username", {
+							required: {
+								value: true,
+								message: "username is required"
+							}
+						})}
+					/>
+					<p className="error">{errors.username?.message}</p>
+				</div>
+
+				<div className="form-control">
+					<input
+						type="email"
+						id="email"
+						placeholder="email"
+						{...register("email", {
+							pattern: {
+								value: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+								message: "Invalid email format"
+							},
+							required: {
+								value: true,
+								message: "email is required"
+							}
+						})}
+					/>
+					<p className="error">{errors.email?.message}</p>
+				</div>
+				<div className="form-control">
+					<input
+						type="text"
+						id="channel"
+						placeholder="channel"
+						{...register("channel", {
+							required: {
+								value: true,
+								message: "channel name is required"
+							}
+						})}
+					/>
+					<p className="error">{errors.channel?.message}</p>
+				</div>
 				<button>Submit</button>
 			</form>
 			<DevTool control={control} />
